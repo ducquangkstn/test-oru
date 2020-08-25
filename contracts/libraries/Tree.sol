8b258b846826fc7aa6ff83054b64079f063e7a8f
ducquangkstn <ducquang.kstn@gmail.com>
2020-08-25 10:49:02 +0700

    add token proof batch

--- contracts/libraries/Tree.sol
+++ contracts/libraries/Tree.sol
@@ -2,16 +2,17 @@ pragma solidity 0.6.6;
 
 import "@nomiclabs/buidler/console.sol";
 
+
 /// @dev https://github.com/zkopru-network/zkopru/blob/1e65597833e47d8b34019c705576debd02765990/packages/contracts/contracts/libraries/Tree.sol
 library RollUpLib {
     uint256 public constant NULL_NODE = 0;
 
     /// @dev get the merkle root from the leaf node and its siblings
-    function merkleAccountRoot(
-        uint256 leaf,
-        uint32 index,
-        uint256[32] memory siblings
-    ) internal pure returns (uint256) {
+    function merkleAccountRoot(uint256 leaf, uint32 index, uint256[32] memory siblings)
+        internal
+        pure
+        returns (uint256)
+    {
         uint32 path = index;
         uint256 node = leaf;
         for (uint256 i = 0; i < siblings.length; i++) {
@@ -46,14 +47,10 @@ library RollUpLib {
             }
             if ((path & 1) == 0) {
                 // right sibling
-                node = uint256(
-                    keccak256(abi.encodePacked(node, tokenProof[i]))
-                );
+                node = uint256(keccak256(abi.encodePacked(node, tokenProof[i])));
             } else {
                 // left sibling
-                node = uint256(
-                    keccak256(abi.encodePacked(tokenProof[i], node))
-                );
+                node = uint256(keccak256(abi.encodePacked(tokenProof[i], node)));
             }
             path >>= 1;
         }
@@ -93,43 +90,23 @@ library RollUpLib {
 
             i = 0;
             while (i < accountIDs.length) {
-                if (
-                    i != accountIDs.length - 1 &&
-                    accountIDs[i] / 2 == accountIDs[i + 1] / 2
-                ) {
+                if (i != accountIDs.length - 1 && accountIDs[i] / 2 == accountIDs[i + 1] / 2) {
                     tmpAccountIDs[accountIndex] = accountIDs[i] / 2;
-                    tmpAccountHashs[accountIndex] = uint256(
-                        keccak256(
-                            abi.encodePacked(
-                                accountHashes[i],
-                                accountHashes[i + 1]
-                            )
-                        )
+                    tmpAccountHashs[accountIndex] = merkleRoot(
+                        accountHashes[i],
+                        accountHashes[i + 1]
                     );
                     i += 2;
                     accountIndex++;
                     continue;
                 }
                 tmpAccountIDs[accountIndex] = accountIDs[i] / 2;
-                if ((accountIDs[i] & 1) == 0) {
-                    tmpAccountHashs[accountIndex] = uint256(
-                        keccak256(
-                            abi.encodePacked(
-                                accountHashes[i],
-                                siblings[siblingIndex]
-                            )
-                        )
-                    );
-                } else {
-                    tmpAccountHashs[accountIndex] = uint256(
-                        keccak256(
-                            abi.encodePacked(
-                                siblings[siblingIndex],
-                                accountHashes[i]
-                            )
-                        )
-                    );
-                }
+                bool isLeft = (accountIDs[i] & 1) == 0;
+                tmpAccountHashs[accountIndex] = merkleRoot(
+                    accountHashes[i],
+                    siblings[siblingIndex],
+                    isLeft
+                );
                 accountIndex++;
                 siblingIndex++;
                 i++;
@@ -138,5 +115,82 @@ library RollUpLib {
             accountHashes = tmpAccountHashs;
         }
         return accountHashes[0];
+    }
+
+    function merkleTokenRoot(
+        uint16[] memory tokenIDs,
+        uint256[] memory tokenHashes,
+        uint256[] memory siblings,
+        uint256 maxDepth
+    ) internal pure returns (uint256) {
+        uint256 siblingIndex = 0;
+        for (uint256 depth = 0; depth < maxDepth; depth++) {
+            uint256 count = 0;
+            uint256 i = 0;
+            uint16[] memory tmpAccountIDs;
+            uint256[] memory tmpAccountHashs;
+            while (i < tokenIDs.length) {
+                if (i == tokenIDs.length - 1) {
+                    count++;
+                    break;
+                }
+
+                if (tokenIDs[i] / 2 == tokenIDs[i + 1] / 2) {
+                    count++;
+                    i += 2;
+                } else {
+                    count++;
+                    i++;
+                }
+            }
+
+            tmpAccountIDs = new uint16[](count);
+            tmpAccountHashs = new uint256[](count);
+            uint256 accountIndex = 0;
+
+            i = 0;
+            while (i < tokenIDs.length) {
+                if (i != tokenIDs.length - 1 && tokenIDs[i] / 2 == tokenIDs[i + 1] / 2) {
+                    tmpAccountIDs[accountIndex] = tokenIDs[i] / 2;
+                    tmpAccountHashs[accountIndex] = merkleRoot(tokenHashes[i], tokenHashes[i + 1]);
+                    i += 2;
+                    accountIndex++;
+                    continue;
+                }
+                tmpAccountIDs[accountIndex] = tokenIDs[i] / 2;
+                bool isLeft = (tokenIDs[i] & 1) == 0;
+                tmpAccountHashs[accountIndex] = merkleRoot(
+                    tokenHashes[i],
+                    siblings[siblingIndex],
+                    isLeft
+                );
+                accountIndex++;
+                siblingIndex++;
+                i++;
+            }
+            tokenIDs = tmpAccountIDs;
+            tokenHashes = tmpAccountHashs;
+        }
+        return tokenHashes[0];
+    }
+
+    function merkleRoot(uint256 current, uint256 sibling, bool isLeft)
+        internal
+        pure
+        returns (uint256)
+    {
+        if (current == 0 && sibling == 0) {
+            return 0;
+        }
+
+        if (isLeft) {
+            return uint256(keccak256(abi.encodePacked(current, sibling)));
+        } else {
+            return uint256(keccak256(abi.encodePacked(sibling, current)));
+        }
+    }
+
+    function merkleRoot(uint256 left, uint256 right) internal pure returns (uint256) {
+        return merkleRoot(left, right, true);
     }
 }
